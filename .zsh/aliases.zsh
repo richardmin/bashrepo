3c76eae3868988fdad2c3d744cd1e00f9cbdf666
Richard Min <richardmin97@gmail.com>
2018-08-30 09:00:15 -0700

    fiinish aliases

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -10,20 +10,30 @@ alias ll='ls -lAFh' # same as above, but with hidden items
 alias lr='ls -tRFh' # l but sorted by date
 alias lt='ls -ltFh' # same as above, but with hidden items
 alias ldot='ls -ld .*' # only hidden items
+alias lS='ls -1FSsh' # show items with biggest first.
+alias lart='ls -1Fcart' # show oldest items first, including hidden items
+alias lrt='ls -1Fcrt' # show oldest items first, no hidden items
 
 alias t='tail -f'
 
 alias grep='grep --color'
 alias sgrep='grep -R -n -H -C 5 --exclude-dir={.git,.svn,CVS} '
 
+alias help='man'
+alias h='history'
+
+
+alias -g H='| head'
+alias -g T='| tail'
+alias -g G='| grep'
+alias -g L="| less"
+alias -g M="| most"
+alias -g LL="2>&1 | less"
+alias -g CA="2>&1 | cat -A"
+alias -g NE="2> /dev/null"
+alias -g NUL="> /dev/null 2>&1"
+alias -g P="2>&1| pygmentize -l pytb"
+
 if [[ "$OSTYPE" == "darwin"* ]]; then
     alias bat="pmset -g batt"
 fi
-
-
-
-
-
-
-
-
