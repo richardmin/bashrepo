3846e0d90c5b9db806e951d8dafa562757cbafcb
Richard Min <richardmin97@gmail.com>
2018-09-10 00:09:07 -0700

    added more completion patterns

--- .zsh/autocomplete.zsh
+++ .zsh/autocomplete.zsh
@@ -1,4 +1,19 @@
 autoload -Uz compinit
-compinit
+compinit -d "$ZSH_CACHE/zcompdump"
+zstyle ':completion::complete:*' use-cache 1
+zstyle ':completion::complete:*' cache-path $ZSH_CACHE
+
+# description for options with exactly one argument
 zstyle ':completion:*' auto-description 'specify: %d'
 
+# match uppercase from lowercase	
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
+
+# Make zsh know about hosts already accessed by SSH	
+zstyle -e ':completion:*:(ssh|scp|sftp|rsh|rsync):hosts' hosts 'reply=(${=${${(f)"$(cat {/etc/ssh_,~/.ssh/known_}hosts(|2)(N) /dev/null)"}%%[# ]*}//,/ })'
+
+zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
+zstyle ':completion:*' list-colors ''
+
+zstyle ':completion:*' list-prompt '%SAt %p: Hit TAB for more, or the character to insert%s'
+zstyle ':completion:*' select-prompt '%SScrolling active: current selection at %p%s'
--- .zsh/theme.zsh
+++ .zsh/theme.zsh
@@ -1,1 +1,4 @@
+
+zstyle ':vcs_info:*' disable-patterns "${(b)HOME}/.zsh(|/*)"
+
 source ~/.zsh/powerlevel9k/powerlevel9k.zsh-theme
