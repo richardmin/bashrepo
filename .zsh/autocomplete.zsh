dd2ba3b5fe90244d9bb15edd3180f2855fd8278f
Richard Min <richardmin97@gmail.com>
2018-09-12 00:39:47 -0700

    More matchers

--- .zsh/autocomplete.zsh
+++ .zsh/autocomplete.zsh
@@ -12,8 +12,13 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 # Make zsh know about hosts already accessed by SSH	
 zstyle -e ':completion:*:(ssh|scp|sftp|rsh|rsync):hosts' hosts 'reply=(${=${${(f)"$(cat {/etc/ssh_,~/.ssh/known_}hosts(|2)(N) /dev/null)"}%%[# ]*}//,/ })'
 
-zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
 zstyle ':completion:*' list-colors ''
 
 zstyle ':completion:*' list-prompt '%SAt %p: Hit TAB for more, or the character to insert%s'
 zstyle ':completion:*' select-prompt '%SScrolling active: current selection at %p%s'
+
+
+zstyle ':completion:*' completer _expand _complete _correct _approximate
+
+zstyle ':completion:*' menu select=2 _expand _complete _correct _approximate
+zstyle ':completion:*' menu select=long
--- .zsh/theme.zsh
+++ .zsh/theme.zsh
@@ -1,4 +1,33 @@
+# Print the name of the git working tree's root directory
+# https://stackoverflow.com/questions/957928/is-there-a-way-to-get-the-git-root-directory-in-one-command/38852055#38852055
+function git_root() {
+  local root first_commit
+  # git displays its own error if not in a repository
+  root=$(git rev-parse --show-toplevel) || return
+  if [[ -n $root ]]; then
+    echo $root
+    return
+  elif [[ $(git rev-parse --is-inside-git-dir) = true ]]; then
+    # We're inside the .git directory
+    # Store the commit id of the first commit to compare later
+    # It's possible that $GIT_DIR points somewhere not inside the repo
+    first_commit=$(git rev-list --parents HEAD | tail -1) ||
+      echo "$0: Can't get initial commit" 2>&1 && false && return
+    root=$(git rev-parse --git-dir)/.. &&
+      # subshell so we don't change the user's working directory
+    ( cd "$root" &&
+      if [[ $(git rev-list --parents HEAD | tail -1) = $first_commit ]]; then
+        pwd
+      else
+        echo "$FUNCNAME: git directory is not inside its repository" 2>&1
+        false
+      fi
+    )
+  else
+    echo "$FUNCNAME: Can't determine repository root" 2>&1
+    false
+  fi
+}
 
-zstyle ':vcs_info:*' disable-patterns "${(b)HOME}/.zsh(|/*)"
 
 source ~/.zsh/powerlevel9k/powerlevel9k.zsh-theme
